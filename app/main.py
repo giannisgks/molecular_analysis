25cee032d00fdf4ab22f80b6eee0f132e12c9db6
giannisgks <giangiakis@gmail.com>
2025-05-13 11:52:38 +0300

    New main script.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import scanpy as sc
 import pandas as pd
 import tempfile
+import scipy
 
 st.set_page_config(page_title="scRNA-seq App", layout="wide")
 
@@ -12,22 +13,53 @@ if "uploaded_file" not in st.session_state:
 # Create tab layout
 tabs = st.tabs(["Upload Data", "Preprocessing", "Single cell RNA", "Differential Expression Analysis", "About Us"])
 
+
+if "selected_tab" not in st.session_state:
+    st.session_state.selected_tab = "Upload Data"
+
 # --------- Tab 1: Upload Data ---------
-with tabs[0]:
-    st.header("Upload Your Data")
+if st.session_state.selected_tab == "Upload Data":
+    with tabs[0]:
+        st.header("Upload Your Data")
 
-    uploaded_file = st.file_uploader("Upload your `.h5ad` file", type=["h5ad"])
+        uploaded_file = st.file_uploader("Upload your `.h5ad` file", type=["h5ad"])
 
-    if uploaded_file:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".h5ad") as tmp_file:
-            tmp_file.write(uploaded_file.read())
-            tmp_path = tmp_file.name
+        if uploaded_file:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".h5ad") as tmp_file:
+                tmp_file.write(uploaded_file.read())
+                tmp_path = tmp_file.name
 
-        adata = sc.read(tmp_path)
-        st.session_state.adata = adata
+            adata = sc.read(tmp_path)
+            st.session_state.adata = adata
 
-        st.subheader("Batch Value Counts")
-        if "batch" in adata.obs:
-            st.write(adata.obs["batch"].value_counts())
-        else:
-            st.warning("'batch' column not found in `adata.obs`.")
+            if "batch" in adata.obs:
+                st.success("✅ File successfully uploaded and loaded.")
+
+                st.subheader("Shape of expression matrix:")
+                st.write(f"{adata.shape[0]} cells x {adata.shape[1]} genes")
+
+                st.subheader("Dataset columns:")
+                st.markdown("<br>".join(adata.obs.columns), unsafe_allow_html=True)
+
+                st.subheader("UMAP Available:")
+                if 'X_umap' in adata.obsm:
+                    st.write("Yes, it is.")
+                else:
+                    st.warning("❌ UMAP is not available. You need to preprocess the data before visualization.")
+
+                st.subheader("Is the matrix sparse?")
+                if (scipy.sparse.issparse(adata.X)):
+                    st.success("✅ The matrix is sparse.")
+                else:
+                    st.warning("❌ The matrix is not sparse. You need to preprocess the data before visualization.")
+                
+                st.subheader("Number of genes to display")
+                selected_gene_count = st.slider("",5, 100, 10)
+                st.write(adata.var_names[:selected_gene_count])
+
+                if st.button("Proceed to Preprocessing"):
+                    st.session_state.selected_tab = "Preprocessing"
+                    st.rerun()
+
+            else:
+                st.warning("'batch' column not found in `adata.obs`.")
